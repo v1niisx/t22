1eb4267a1310e99e753bb5b8299519e76da281e3
Giovanalupo <130668804+Giovanalupo@users.noreply.github.com>
2023-10-17 07:22:19 -0300

    triangulo commit22

--- Projeto Triangulo.py
+++ Projeto Triangulo.py
@@ -1,25 +1,14 @@
-#Validador de Triangulo
-
+#Validar Triangulo
 print('\n\t\t --- Validador de Triangulo --- \n\t\t')
-
 #Entradas
-
-la = float(input('Digite o Valor do Lado A:\n').strip())
-lb = float(input('Digite o Valor do Lado B:\n').strip())
-lc = float(input('Digite o Valor do Lado C:\n').strip())
-
+la = float(input('Digite o Valor do Lado A: ').strip())
+lb = float(input('Digite o Valor do Lado B: ').strip())
+lc = float(input('Digite o Valor do Lado C: ').strip())
 #Processamento e Saida
-
 lp = la < (lb+lc) and lb < (la+lc) and lc < (la + lb)
 
 if lp == True:
     print('É um Triangulo Válido!')
-    if la == lb and la == lc:
-        print('Este triangulo é Equilátero!')
-    elif la and lb == la:
-        print('Este triangulo é Isósceles')
-    else:
-        print('Este triangulo é Escaleno')
+
 else:
     print('Não é um Triangulo válido')
-
